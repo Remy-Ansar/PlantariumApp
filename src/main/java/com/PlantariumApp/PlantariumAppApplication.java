dcc337ac1555240d5da125cead2b3bb01ce1dc78
Remosaure <remy.ansar@protonmail.com>
2026-07-02 16:01:23 +0200

    Init(project) project initialization

---
+++ src/main/java/com/PlantariumApp/PlantariumAppApplication.java
@@ -0,0 +1,13 @@
+package com.PlantariumApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantariumAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantariumAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PlantariumApp/PlantariumAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PlantariumApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantariumAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
